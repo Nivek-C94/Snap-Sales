65cee890253fbac75d17e5f587191680cfbd0e58
Kevin <carlisle1147@gmail.com>
2024-04-23 11:10:33 -0400

    Updated file

--- src/CameraService.java
+++ src/CameraService.java
@@ -5,8 +5,12 @@ import android.content.Context;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CameraCaptureSession;
 import android.util.Log;
 import android.view.Surface;
+import java.util.Arrays;
+import androidx.annotation.NonNull;
 
 public class CameraService {
     private CameraManager cameraManager;
@@ -52,5 +56,34 @@ public class CameraService {
     // Start the camera preview
     private void startPreview() {
         // Code to start the camera preview
+    }
+
+    // Method to start continuous capture
+    public void startContinuousCapture() {
+        try {
+            // Create a new capture request for continuous capture
+            CaptureRequest.Builder captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            captureBuilder.addTarget(previewSurface); // Assuming previewSurface is correctly set up
+            captureBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+
+            // Start the camera preview session for continuous capture
+            cameraDevice.createCaptureSession(Arrays.asList(previewSurface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    try {
+                        session.setRepeatingRequest(captureBuilder.build(), null, null);
+                    } catch (CameraAccessException e) {
+                        Log.e("CameraService", "Failed to start continuous capture", e);
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                    Log.e("CameraService", "Configuration change failed");
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            Log.e("CameraService", "Failed to access camera for continuous capture", e);
+        }
     }
 }
